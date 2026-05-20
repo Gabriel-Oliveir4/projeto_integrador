29152b22cbe922a9d52fcfd862cbeb677f3852b0
Gabriel-Oliveir4 <g4briel.jaime.oliveira@gmail.com>
2026-05-19 21:23:52 -0300

    feat: teste

--- clinica/proxy.ts
+++ clinica/proxy.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 
 
 //verifica se tem tooken, se nao tiver jooga proo login
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
   const { pathname } = request.nextUrl;
 
